b17484179623238c407074307959b9ef29b355d6
lennardvandenberg <lennosberg@gmail.com>
2021-12-19 10:32:25 +0100

    Added Next.js Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: '@lennardvdberg/eslint-config-react-lennard',
+  extends: ['@lennardvdberg/eslint-config-react-lennard', 'plugin:@next/next/recommended'],
   rules: {
     // turn off href checks on <a/> elements, because of Next.js Link component
     'jsx-a11y/anchor-is-valid': [
@@ -10,5 +10,6 @@ module.exports = {
         aspects: ['invalidHref', 'preferButton'],
       },
     ],
+    'react/function-component-definition': ['error', {namedComponents: 'arrow-function'}],
   },
 }
